67058cfd43c68469f6d9c3589435c57e69daf9a7
MD EKRAM <Mekram47@gmail.com>
2024-02-02 11:52:04 +0530

    Discussion About the utils folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // require ('dotenv').config({path:'./env'}) // import method is below
 import dotenv from "dotenv";
+import app from "./app.js"
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants"; // THis is not used as second approach is done 
@@ -10,6 +11,14 @@ dotenv.config({path:'./env'})
 
 
 connectDB()
+.then(()=>[
+    app.listen(process.env.PORT||7000,()=>{
+        console.log(`Server is listening at Port no ${process.env.PORT}`);
+    })
+])
+.catch((err)=>{
+console.log("MONGODB failed to Connect!!!!!!!!!!",err);
+})
 
 //  ---------------------------This is First Approach Of MONGODB connection------------------------------------
 // import  express  from "express";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,45 @@
+const asyncHandler= (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}  
+
+
+// -----------------------------------------------THis is first Method of Promises--------------------------------
+
+
+
+// -----------------------------------------THis is Second  Method of try and catch--------------------------------
+
+
+
+// const asyncHandler=()=>{}                    // Now Showing How we can write Super Order Function
+
+// const asyncHandler =()=>{()=>{}} //           Here We can REmove Curly Brackets to make it super orderfunc
+
+// const asyncHandler =()=>()=>{}                  // Given is the format of super order function//
+
+
+
+
+
+//  const asyncHandler =(fn)=>async(req,res,next)=>{
+
+//     try {
+//         await fn(req,res,next)
+
+        
+//     } catch (err) {
+//         res.status(err.code||400).json({
+//             success:false,
+//             message:err.message
+//         })
+        
+//     }
+//  }
+
+
+
+ export default asyncHandler
+
+
